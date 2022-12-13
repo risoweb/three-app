63da850b3841786890da65fef7bec69a43bade29
Rick <ilson.ricardo@gmail.com>
2022-12-12 23:42:38 -0300

    Start git repository

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { useRef} from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { Model } from "./Model";
+import { OrbitControls } from '@react-three/drei'
 import "./styles.scss";
 
-const Box = () => {
-  const mesh = useRef(null);
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-  return (
-<mesh ref={mesh}>
-  <cylinderGeometry attach='geometry' args={[1, 1, 1]} />
-  <meshPhongMaterial attach="material" color="#a6fb22" />
-</mesh>
-  );
-}
+
 
 function App() {
   
   return (
     <>
-      <Canvas>
-        <Box />
+      <Canvas>        
         <ambientLight args={[0xff0000]} intensity={0.01} />
         <directionalLight position={[0, 0, 5]} intensity={0.5} />
-       
+        <directionalLight position={[0, 0, -5]} intensity={0.5} />
+        <directionalLight position={[0, 10, 0]} intensity={0.3} />
+        <Suspense fallback={null}><Model /></Suspense>
+        <OrbitControls autoRotate enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.8} /> 
       </Canvas>
     </>
   );
